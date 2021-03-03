cdbadad09b6a8173cfce3317de9a3184d62c1c61
SerebryakovS <serebryakov0906@gmail.com>
2021-03-04 01:57:18 +0600

    sensor.py completed

--- sensor.py
+++ sensor.py
@@ -1,10 +1,12 @@
-import os,tempfile
-from time import sleep,time
+import os
+import sys
+import json
+import time
+import random
 from queue import Queue
 from random import randrange
 from threading import Thread
-
-
+#--------------------------------------------------------------
 class sensor(Thread):
     def __init__(self, tasks):
         Thread.__init__(self)
@@ -20,38 +22,66 @@ class sensor(Thread):
                 print(ex)
             finally:
                 self.tasks.task_done()
-
+#--------------------------------------------------------------
 class sensors_pool(object):
-    def __init__(self, num_threads):
-        self.tasks = Queue(num_threads)
+    def __init__(self, num_threads,sensors_folder,fifo_prefix):
+        self.tasks = Queue(num_threads);
+        self.sensors_folder = sensors_folder;
+        fifos_arr = list();
+        for i in range(num_threads):
+            fifos_arr.append(
+                os.path.join(self.sensors_folder,fifo_prefix+str(i)));
+            try:
+                curr_fifo = fifos_arr[-1];
+                print("creating FIFO: %s"%curr_fifo);
+                os.mkfifo(curr_fifo);
+            except Exception as ex:
+                print("Failed to create FIFO: %s"%ex);
         for _ in range(num_threads):
             sensor(self.tasks)
     def add_task(self, func, *args, **kargs):
         self.tasks.put((func, args, kargs))
-    def map(self, func, args_list):
-        for args in args_list:
-            self.add_task(func, args)
+    def map(self, func, args_list1, args_list2, args_list3):
+        for arg_index in range(len(args_list1)):
+            self.add_task(func, args_list1[arg_index],
+                                args_list2[arg_index],
+                                args_list3[arg_index])
     def wait_completion(self):
-        self.tasks.join()
-
+        self.tasks.join();
+        os.system("rm -rf %s/*"%self.sensors_folder);
+#--------------------------------------------------------------
 if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Usage : %s seconds_to_work"%sys.argv[0]);
+        exit(-1);
     from get_config import get_config
     params = get_config();
-    def run_sensor(freq):
-        start_time = time();
-        sleep(1);
-        print(time()-start_time);
-        
-        #print("sleeping for (%d)sec" % d)
-        #sleep(d)
-    numb = int(params['SENSORS_NUMBER']);
-    freq = [int(params['SENSOR_FREQUENCY']) for i in range(numb)]
-    pool = sensors_pool(numb);
-    pool.map(run_sensor,freq);
+    def run_sensor(freq, filename, seconds_to_work):
+        sensor_id = filename.split("_")[1];
+        sensor_IO = open(filename,'w');
+        start_time = time.time();
+        waiting_time = 0;
+        while seconds_to_work - (time.time()-start_time) >= 0:
+            time.sleep(1.0/freq);
+            message = json.dumps({"sens_num": sensor_id,
+                                  "datetime": time.localtime(),
+                                  "payload" : random.randint(1,1000)})
+            sensor_IO.write(message+"\n");
+        sensor_IO.close();
+        os.remove(filename); 
+    nb_sensors = int(params['SENSORS_NUMBER']);
+    sensors_folder = params['SENSORS_FOLDER'];
+    try:
+        os.mkdir(sensors_folder);
+    except: pass;
+    fifo_prefix = "sensor_";
+    filename = [sensors_folder+"/"+fifo_prefix+str(i) for i in range(nb_sensors)];
+    freq = [int(params['SENSOR_FREQUENCY']) for i in range(nb_sensors)];
+    secs = [int(sys.argv[1]) for i in range(nb_sensors)];
+    pool = sensors_pool(nb_sensors,sensors_folder,fifo_prefix);
+    pool.map(run_sensor,freq,filename,secs);
     pool.wait_completion()
-
-
-
+#--------------------------------------------------------------
 
 
 
